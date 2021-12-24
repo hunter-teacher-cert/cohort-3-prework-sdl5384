d3f25f6802ed827943f5d769da89545f53b65a33
sdl5384 <96607503+sdl5384@users.noreply.github.com>
2021-12-24 05:36:58 +0000

    I made some changes in the errors and fixed the assignment errors.

---
+++ .github/pre02/Date.java
@@ -0,0 +1,12 @@
+public class Date
+{
+  public static void main(String[] args)
+  {
+    String day;
+    int month, date, year;
+    day = "Friday";
+    date = 24;
+    month = 12;
+    year = 2021;
+  }
+}
