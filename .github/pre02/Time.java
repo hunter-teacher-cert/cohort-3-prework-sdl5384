a8deb0297dce8acfcbce1815d285c175580a4bf3
sdl5384 <96607503+sdl5384@users.noreply.github.com>
2021-12-28 18:38:38 +0000

    I put in print lines so we can see what the values of each of the variables are.

---
+++ .github/pre02/Time.java
@@ -0,0 +1,16 @@
+public class Time
+{
+  public static void main(String[] args)
+  {
+    String day;
+    int month, date, year;
+    day = "Friday";
+    System.out.println("day = " + day);
+    date = 24;
+    System.out.println("date = " + date);
+    month = 12;
+    System.out.println("month = " + month);
+    year = 2021;
+    System.out.println("year = " + year);
+  }
+}
