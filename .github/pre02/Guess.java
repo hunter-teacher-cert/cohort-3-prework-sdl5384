ef928d8b5e2d4fee73c4a8d603b8e5b35e1f0e4c
sdl5384 <96607503+sdl5384@users.noreply.github.com>
2021-12-29 16:27:58 +0000

    I completed the Guess program.  I realized that Random was already contained in the java.util package so I could just use the import java.util.* line to account for both the Scanner class and the Random class.
    I also realized that it could be possible that the user could pick a number that is greater than the randomly chosen value, so it may spit out a negative value.  To account for that I used the Math class and used the absolute value method to make sure the difference stayed positive.

---
+++ .github/pre02/Guess.java
@@ -0,0 +1,25 @@
+import java.util.*;
+//import java.Random.*;
+import java.lang.Math.*;
+
+public class Guess
+{
+  public static void main(String[] args)
+  {
+    System.out.println("I'm thinking of a number between 1 and 100 (including both).  Can you guess what it is?");
+    System.out.print("Guess a number: ");
+
+    Scanner input = new Scanner(System.in);
+    int userGuess = input.nextInt();
+    input.nextLine();
+    System.out.println("Your guess was: " + userGuess);
+
+    Random random = new Random();
+    //pick a random value
+    int number = random.nextInt(100)+1;
+    int diff = Math.abs(number - userGuess);
+
+    System.out.println("The number I was thinking of is: " + number);
+    System.out.println("You were off by " + diff);
+  }
+}
