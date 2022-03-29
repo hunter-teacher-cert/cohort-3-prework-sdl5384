d3e4ddc8597cecb3b038aa7e4260d88b8c4b57d9
sdl5384 <96607503+sdl5384@users.noreply.github.com>
2022-03-29 00:24:21 +0000

    Some revisions being done to the final project code

--- .github/pre08/Craps.java
+++ .github/pre08/Craps.java
@@ -3,10 +3,41 @@ import java.lang.Math.*;
 
 public class Craps
 {
+  //array sequence will be win, lose, point
+  int[] player_data = new int[3];
+
+  public static void setup_player_data()
+  {
+    this.player_data[0] = 1;
+    this.player_data[1] = 1;
+    this.player_data[2] = 1;
+  }
+
+  public static int[] get_player_data()
+  {
+    return this.player_data;
+  }
+
+  public static void counter(String game_point)
+  {
+    if (game_point.equals("win"))
+    {
+      this.player_data[0]++;
+    }
+    else if (game_point.equals("lose"))
+    {
+      this.player_data[1]++;
+    }
+    else if (game_point.equals("point"))
+    {
+      this.player_data[2]++;
+    }
+  }
+  
   public static int[] roll()
   {
-    int[] die_one = [1,2,3,4,5,6];
-    int[] die_two = [1,2,3,4,5,6];
+    int[] die_one = {1,2,3,4,5,6};
+    int[] die_two = {1,2,3,4,5,6};
 
     Random random = new Random();
     int[] dice_values = new int[2];
@@ -35,19 +66,25 @@ public class Craps
     }
     return result;
   }
-
-  public static 
   
-  public static round()
+  public static void round()
   {
     int[] dice_numbers = new int[2];
     dice_numbers = roll();
     String player_status = shoot(dice_numbers);
     int number_of_rounds = 1;
-    while (player)
   }
   public static void main(String[] args)
   {
-    
+    String status = round();
+    this.counter(status);
+    while (!status.equals("win") || !status.equals("lose"))
+    {
+      status.round();
+      this.counter(status);
+    }
+
+    int[] data = this.get_player_data();
+    System.out.println("")
   }
 }
