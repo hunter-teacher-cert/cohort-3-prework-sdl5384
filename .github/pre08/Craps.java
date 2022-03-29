21860e039057d619f7db74cbad8551d087504d7b
sdl5384 <96607503+sdl5384@users.noreply.github.com>
2022-03-29 00:59:22 +0000

    revisions...

--- .github/pre08/Craps.java
+++ .github/pre08/Craps.java
@@ -3,37 +3,6 @@ import java.lang.Math.*;
 
 public class Craps
 {
-  //array sequence will be win, lose, point
-  int[] player_data = new int[3];
-
-  public static void setup_player_data()
-  {
-    this.player_data[0] = 1;
-    this.player_data[1] = 1;
-    this.player_data[2] = 1;
-  }
-
-  public static int[] get_player_data()
-  {
-    return this.player_data;
-  }
-
-  public static void counter(String game_point)
-  {
-    if (game_point.equals("win"))
-    {
-      this.player_data[0]++;
-    }
-    else if (game_point.equals("lose"))
-    {
-      this.player_data[1]++;
-    }
-    else if (game_point.equals("point"))
-    {
-      this.player_data[2]++;
-    }
-  }
-  
   public static int[] roll()
   {
     int[] die_one = {1,2,3,4,5,6};
@@ -47,7 +16,7 @@ public class Craps
     return dice_values;
   }
 
-  public static String shoot(int[] dice_vals)
+  public static Object[] shoot(int[] dice_vals)
   {
     String result = "";
     int sum = dice_vals[0] + dice_vals[1];
@@ -64,7 +33,11 @@ public class Craps
     {
       result = "point";
     }
-    return result;
+    System.out.println("You have rolled a " + dice_vals[0] + " and a " + dice_vals[1]);
+    System.out.println("Your dice sum is " + sum);
+
+    Object[] game_result = {result, dice_vals[0], dice_vals[1]};
+    return game_result;
   }
   
   public static void round()
@@ -72,19 +45,47 @@ public class Craps
     int[] dice_numbers = new int[2];
     dice_numbers = roll();
     String player_status = shoot(dice_numbers);
-    int number_of_rounds = 1;
   }
+
+  public static int[] counter(String game_point)
+  {
+    //array sequence is Win, Lose, Point
+    int[] player_data = new int[3];
+    player_data[0] = 1;
+    player_data[1] = 1;
+    player_data[2] = 1;
+    if (game_point.equals("win"))
+      player_data[0]++;
+    else if (game_point.equals("lose"))
+      player_data[1]++;
+    else if (game_point.equals("point"))
+      player_data[2]++;
+
+    return player_data;
+  }
+  
   public static void main(String[] args)
   {
-    String status = round();
-    this.counter(status);
-    while (!status.equals("win") || !status.equals("lose"))
+    Object[] status = round();
+    int sum = status[1] + status[2];
+    this.counter(status[0]);
+    while (!status[0].equals("win") || !status[0].equals("lose"))
     {
-      status.round();
-      this.counter(status);
+      status = round();
+      counter(status[0]);
+      if (status.equals("point"))
+      {
+        status = round();
+        counter(status[0]);
+        if (status[0].equals("point") || sum == 7)
+        {
+          status[0] = "win";
+          counter(status[0]);
+        }
+      }
     }
 
     int[] data = this.get_player_data();
-    System.out.println("")
+    System.out.println("");
   }
 }
